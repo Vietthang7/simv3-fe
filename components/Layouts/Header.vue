2d689ef88ef248b0e8c058afb224a1e258503f34
thangnguyen <nguyenvietthang12092004@gmail.com>
2025-04-11 00:25:54 +0700

    Refactor code structure for improved readability and maintainability

--- components/Layouts/Header.vue
+++ components/Layouts/Header.vue
@@ -1,7 +1,34 @@
 <script setup>
-const route = useRoute();
 const { restAPI } = useApi();
-console.log("restAPI", restAPI)
+const props = defineProps({
+  categories: {
+    type: Array,
+    default: () => [],
+  },
+});
+const searchQuery = ref(null);
+const searchResults = ref([]);
+const resultSearch = ref([]);
+let searchTimeout = null;
+const searchInput = ref(null);
+
+const route = useRoute();
+
+const callApiSearch = async () => {
+  // Gọi API để lấy dữ liệu
+  if (resultSearch.value.length === 0) {
+    const { data: resData } = await restAPI.stores.getSearch();
+    resultSearch.value = resData.data;
+  }
+}
+const handleSearch = () => {
+  const searchQueryTrimmed = searchQuery.value.trim().toLowerCase();
+  if (searchQueryTrimmed === "") {
+    return [];
+  }
+
+}
+const toggleMenu = () => {}
 </script>
 
 <template>
@@ -10,11 +37,13 @@ console.log("restAPI", restAPI)
       <div class="sticky top-0 z-[45] w-full">
         <div class="w-full lg:w-auto py-2 gap-4 bg-primary text-center z-60">
           <h1 class="text-white text-base font-light" v-if="['/'].includes(route.path)">
-
+            Sim247 - Sim du lịch quốc tế
           </h1>
+          <p class="text-white text-base font-light" v-else>
+            Sim247 - Sim du lịch quốc tế
+          </p>
         </div>
       </div>
-
     </div>
 
   </div>
