1f8d7b755ea893b66ad79b7a8583f9413d14e170
thangnguyen <nguyenvietthang12092004@gmail.com>
2025-04-23 01:24:25 +0700

    feat: add Banner and index components with API integration and SEO metadata

---
+++ components/Base/Banner.vue
@@ -0,0 +1,6 @@
+<script setup>
+const { restAPI } = useApi();
+const route = useRoute();
+const img = useImage();
+const
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+const { restAPI } = useApi();
+const config = useRuntimeConfig();
+const {data : resData} = await restAPI.stores.getListProductCategory({
+  params: {
+    current: 1,
+    pageSize: 9999,
+  },
+});
+const productCategoryList = resData.value.data ?? [];
+
+useHead({
+  title: "Sim247 - Sim du lịch quốc tế",
+  link: [{ ref: "canonical", href: config.public.domainUrl }],
+})
+// rel="canonical" giúp tránh nội dung trùng lặp và cải thiện thứ hạng trang chính thức
+useSeoMeta({
+  title: "Sim247 - Sim du lịch quốc tế",
+  description:
+      "Sim247 - Sim du lịch quốc tế, Chuyên sim du lịch giá rẻ giá ngon đảm bảo chất lượng",
+  keywords: "sim du lịch, sim du lịch giá rẻ, sim du lịch chất lượng",
+});
+</script>
+
+
+<template>
+  <div>
+
+  </div>
+</template>
